a6834cb700c8c6b5c135ff64a01a01fbde9c4eb9
AmiSora93 <lehuyenlinh09@gmail.com>
2024-03-26 00:47:28 +0700

    add order table and columns for student

--- web_isd.sql
+++ web_isd.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 20, 2024 at 04:33 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Generation Time: Mar 25, 2024 at 06:43 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,12 +29,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `student` (
   `mhv` varchar(6) NOT NULL,
-  `ho_ten` varchar(50) DEFAULT NULL,
+  `ho_ten` varchar(50) NOT NULL,
   `ngay_sinh` date DEFAULT NULL,
   `ho_chieu` varchar(8) DEFAULT NULL,
   `CCCD` varchar(12) DEFAULT NULL,
   `que_quan` varchar(50) DEFAULT NULL,
-  `sdt` varchar(10) DEFAULT NULL,
+  `sdt` varchar(10) NOT NULL,
   `ngay_thi` date DEFAULT NULL,
   `co_quan` varchar(50) DEFAULT NULL,
   `ngay_DKXC` date DEFAULT NULL,
@@ -44,38 +44,17 @@ CREATE TABLE `student` (
   `xi_nghiep` varchar(50) DEFAULT NULL,
   `nghiep_doan` varchar(50) DEFAULT NULL,
   `noi_lam_viec` varchar(50) DEFAULT NULL,
-  `note` varchar(200) DEFAULT NULL
+  `note` varchar(200) DEFAULT NULL,
+  `sdt_nguoigiamho` int(11) NOT NULL,
+  `trang_thai` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`mhv`, `ho_ten`, `ngay_sinh`, `ho_chieu`, `CCCD`, `que_quan`, `sdt`, `ngay_thi`, `co_quan`, `ngay_DKXC`, `ngayXC`, `dukien_venuoc`, `nganh_nghe`, `xi_nghiep`, `nghiep_doan`, `noi_lam_viec`, `note`) VALUES
-('MHV001', 'Nguyễn Văn A', '1990-05-20', '12345678', 'CCCD123456', 'Hà Nội', '0987654321', '2024-03-15', 'Công ty ABC', '2024-01-01', '2024-02-01', '2024-04-01', 'Kỹ sư phần mềm', 'Công ty ABC', 'Kỹ sư', 'Hà Nội', 'Ghi chú 1'),
-('MHV002', 'Trần Thị B', '1995-10-10', '87654321', 'CCCD654321', 'Hồ Chí Minh', '0123456789', '2024-03-20', 'Công ty XYZ', '2024-02-01', '2024-03-01', '2024-05-01', 'Chuyên viên marketing', 'Công ty XYZ', 'Nhân viên', 'Hồ Chí Minh', 'Ghi chú 2');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(12) NOT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `email` varchar(50) NOT NULL,
-  `user` varchar(20) NOT NULL,
-  `pass` varchar(20) NOT NULL,
-  `role` int(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `name`, `email`, `user`, `pass`, `role`) VALUES
-(1, 'Admin', 'admin@gmail.com', 'admin', '123', 1);
+INSERT INTO `student` (`mhv`, `ho_ten`, `ngay_sinh`, `ho_chieu`, `CCCD`, `que_quan`, `sdt`, `ngay_thi`, `co_quan`, `ngay_DKXC`, `ngayXC`, `dukien_venuoc`, `nganh_nghe`, `xi_nghiep`, `nghiep_doan`, `noi_lam_viec`, `note`, `sdt_nguoigiamho`, `trang_thai`) VALUES
+('MHV002', 'Trần Thị B', '1995-10-10', '87654321', 'CCCD654321', 'Hồ Chí Minh', '0123456789', '2024-03-20', 'Công ty XYZ', '2024-02-01', '2024-03-01', '2024-05-01', 'Chuyên viên marketing', 'Công ty XYZ', 'Nhân viên', 'Hồ Chí Minh', 'Ghi chú 2', 0, '');
 
 --
 -- Indexes for dumped tables
@@ -86,12 +65,6 @@ INSERT INTO `user` (`id`, `name`, `email`, `user`, `pass`, `role`) VALUES
 --
 ALTER TABLE `student`
   ADD PRIMARY KEY (`mhv`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
